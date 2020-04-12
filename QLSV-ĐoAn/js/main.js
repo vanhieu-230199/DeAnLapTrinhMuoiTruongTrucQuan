5380bb871ff11c345d03d5686edfac753d1c4277
LeKiet2369 <55068146+LeKiet2369@users.noreply.github.com>
2020-04-12 09:11:22 +0700

    Update-03 Kiet

--- QLSV-ĐoAn/js/main.js
+++ QLSV-ĐoAn/js/main.js
@@ -18,3 +18,26 @@ inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
 	input.addEventListener("blur", remcl);
 });
+// funtion move form 
+		var x = document.getElementById("student");
+		var y = document.getElementById("teacher");
+		//teacher.style.color = "white";
+        var z = document.getElementById("moveform");
+
+        function teacher(){
+          x.style.left = "-400px";
+          y.style.top =  "-300px";
+		  z.style.left = "110px";
+		  teacherId.style.color = "white";
+		  studentId.style.color = "black";
+        }
+
+        function student(){
+          x.style.left = "0px";
+          y.style.top =  "300px";
+		  z.style.left = "0";
+		  studentId.style.color = "white";
+		  teacherId.style.color = "black";
+		}
+		
+		
