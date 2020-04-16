431e15a9097d0af922ab7aeb0ed95a84ece8851d
LeKiet2369 <55068146+LeKiet2369@users.noreply.github.com>
2020-04-16 20:21:42 +0700

    Updte-5-Kiet

--- GDUManagement/Web/QLSV-ĐoAn/js/main.js
+++ GDUManagement/Web/QLSV-ĐoAn/js/main.js
@@ -18,11 +18,12 @@ inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
 	input.addEventListener("blur", remcl);
 });
-// funtion move form 
+
+// start funtion move form 
 		var x = document.getElementById("student");
 		var y = document.getElementById("teacher");
-		//teacher.style.color = "white";
-        var z = document.getElementById("moveform");
+		var z = document.getElementById("moveform");
+		
 
         function teacher(){
           x.style.left = "-400px";
@@ -39,5 +40,9 @@ inputs.forEach(input => {
 		  studentId.style.color = "white";
 		  teacherId.style.color = "black";
 		}
-		
-		
+//end funtion move form login 
+
+function XemDiem(){
+	var divPoint = document.getElementById('divPoint');
+	divPoint.style.display = "block";
+}
